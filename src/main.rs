8533f24af43636aea2efec8afa7b62022ab61bef
Adrian Dawid <adriandwd@gmail.com>
2021-01-01 18:45:51 +0100

    Add basic cli options parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,33 @@
+use structopt::StructOpt;
+use std::str::FromStr;
+use strum_macros::EnumString;
+
+#[derive(Debug, PartialEq, EnumString)]
+enum BuildMode {
+    Presentation, 
+    LectureNotes,
+    Both
+}
+
+#[derive(StructOpt)]
+struct CliOpt {
+    /// the build mode
+    mode: BuildMode, 
+    /// the file name of the TeX main file
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    let pattern = std::env::args(0).expect("no pattern given");
-    println!("Hello, world!");
+    // Load arguments from the command line
+    let args = CliOpt::from_args(); 
+    if args.mode == BuildMode::Both {
+        let content = std::fs::read_to_string(&args.path)
+            .expect("could not read file");
+        for line in content.lines() {
+           if line.contains("@bni") {
+                println!("{}", line);
+            }   
+        }
+    }
 }
