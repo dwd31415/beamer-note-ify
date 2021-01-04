bb5f64e0df1985e6ee86b1638709ea455f4ff200
Adrian Dawid <adriandwd@gmail.com>
2021-01-04 12:16:22 +0100

    Add LaTeX build

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use structopt::StructOpt;
 use std::fs;
-use strum_macros::EnumString;
 use std::fmt;
+use std::ffi::{OsString};
+use std::io::{self, Write};
+use std::process::Command;
+use strum_macros::EnumString;
+use structopt::StructOpt;
 
 mod parser;
 mod compiler;
@@ -31,9 +34,15 @@ struct CliOpt {
     /// the file name of the TeX main file
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
+    /// the LaTeX command
+    #[structopt(short = "tex", long = "latex-executable", default_value = "pdflatex")]
+    pdflatex: String,
+    /// the build folder 
+    #[structopt(short = "o", long = "output", default_value = "build", parse(from_os_str))]
+    build_folder: std::path::PathBuf,
 }
 
-fn build(build_mode : BuildMode, file_name: &std::path::PathBuf){
+fn build(build_mode : BuildMode, file_name: &std::path::PathBuf, pdflatex: &str, build_folder : &std::path::PathBuf){
     let content = std::fs::read_to_string(file_name)
         .expect("Error: Could not read file.");
     let blocks = parser::parse_input(content).expect("Quit because of a parsing error.");
@@ -41,7 +50,27 @@ fn build(build_mode : BuildMode, file_name: &std::path::PathBuf){
     match content_maybe {
         Some(content_final) => {
             let new_path = file_name.with_extension("").with_extension(format!("{}.tex", build_mode)); 
-            fs::write(new_path, content_final).expect("Unable to write the output file.");
+            fs::write(&new_path, content_final).expect("Unable to write the output file.");
+            match fs::create_dir_all(build_folder) {
+                Ok(()) => {},
+                Err(_) => {println!("Could not create output folder. Check permissions.");}
+            }
+            let mut aux_dir = OsString::from("-aux-directory=");
+            aux_dir.push(build_folder.as_os_str());
+            let mut out_dir = OsString::from("-output-directory=");
+            out_dir.push(build_folder.as_os_str());
+            let output = Command::new(pdflatex).
+                args(&[aux_dir.as_os_str(), 
+                       out_dir.as_os_str(),
+                       &new_path.as_os_str()]).output().expect("Could not run LaTeX compiler at the end. Please compile manually.");
+            println!("LaTex build status: {}", output.status);
+            io::stdout().write_all(&output.stdout).unwrap();
+            io::stderr().write_all(&output.stderr).unwrap();
+            if output.status.success() {
+                println!("Finished successfully.");
+            } else {
+                println!("LaTeX compilation failed.");
+            }
         },
         None => { println!("There was a fatal error."); }
     }
@@ -51,8 +80,8 @@ fn main() {
     // Load arguments from the command line
     let args = CliOpt::from_args(); 
     match args.mode {
-        BuildMode::Presentation => {build(args.mode, &args.path);},
-        BuildMode::LectureNotes => {build(args.mode, &args.path);},
-        BuildMode::Both => {build(BuildMode::LectureNotes, &args.path);build(BuildMode::Presentation, &args.path);},
+        BuildMode::Presentation => {build(args.mode, &args.path, args.pdflatex.as_str(), &args.build_folder);},
+        BuildMode::LectureNotes => {build(args.mode, &args.path, args.pdflatex.as_str(), &args.build_folder);},
+        BuildMode::Both => {build(BuildMode::LectureNotes, &args.path, args.pdflatex.as_str(), &args.build_folder);build(BuildMode::Presentation, &args.path, args.pdflatex.as_str(), &args.build_folder);},
     }
 }
